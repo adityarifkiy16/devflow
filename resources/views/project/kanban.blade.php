875f933aaccbec0bef18b19123ae7d2df5ad1e6b
adityarifkiy16 <adityarifkistemba@gmail.com>
2024-06-27 16:11:15 +0700

    fix:change time toast

--- resources/views/project/kanban.blade.php
+++ resources/views/project/kanban.blade.php
@@ -150,7 +150,7 @@
             toast: true,
             position: "top-end",
             showConfirmButton: false,
-            timer: 3000,
+            timer: 2000,
             timerProgressBar: true,
             didOpen: (toast) => {
                 toast.onmouseenter = Swal.stopTimer;
