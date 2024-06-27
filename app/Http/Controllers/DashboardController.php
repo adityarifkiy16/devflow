ec6a23c6af44a13d92156177aa42c91a86279a3d
adityarifkiy16 <adityarifkistemba@gmail.com>
2024-06-27 16:10:13 +0700

    feat: add banner task & project count to dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use App\Models\User;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -10,7 +12,9 @@ class DashboardController extends Controller
     public function __invoke()
     {
         $users = User::with('role')->get();
+        $taskCount = Task::count();
         $userCount = User::count();
-        return view("dashboard.admindashboard", compact('users', 'userCount'));
+        $projectCount = Project::count();
+        return view("dashboard.admindashboard", compact('users', 'userCount', 'taskCount', 'projectCount'));
     }
 }
