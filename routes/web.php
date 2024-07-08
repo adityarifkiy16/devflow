0c65893d09ab058a42ab82ddabec8ef6ae46fd09
adityarifkiy16 <adityarifkistemba@gmail.com>
2024-07-08 09:14:55 +0700

    feat : add role middleware

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,56 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
-use App\Http\Controllers\TaskController;
 
 Route::get('/', function () {
-    return redirect('/login');
+    if (Auth::check()) {
+        return redirect('/dashboard'); // Redirect to the dashboard or any other page
+    } else {
+        return redirect('/login');
+    }
 });
 
-Route::get('/login', [LoginController::class, 'index'])->name('login');
-Route::post('/login', [LoginController::class, 'login']);
+Route::controller(LoginController::class)->group(function () {
+    Route::get('/login', 'index')->name('login');
+    Route::post('/login', 'login');
+});
 
+// Protected routes
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', DashboardController::class)->name('admin.dashboard');
     Route::post('/logout', LogoutController::class)->name('logout');
-    Route::resource('users', UserController::class);
     Route::resource('project', ProjectController::class);
     Route::get('/projects/{project}/kanban', [ProjectController::class, 'kanban'])->name('project.kanban');
 
-    Route::prefix('task')->group(function () {
-        Route::post('/store', [TaskController::class, 'store']);
-        Route::post('/update-task-status', [TaskController::class, 'updateTaskStatus']);
-        Route::post('/update/{id}', [TaskController::class, 'update']);
-        Route::delete('/delete/{id}', [TaskController::class, 'destroy'])->name('task.destroy');
-        Route::get('/edit/{id}', [TaskController::class, 'edit'])->name('task.edit');
+    Route::prefix('task')->controller(TaskController::class)->group(function () {
+        Route::post('/store', 'store');
+        Route::post('/update-task-status', 'updateTaskStatus');
+        Route::post('/update/{id}', 'update');
+        Route::delete('/delete/{id}', 'destroy')->name('task.destroy');
+        Route::get('/edit/{id}', 'edit')->name('task.edit');
+    });
+
+    //full access 
+    Route::middleware(['role:1,2,3'])->group(function () {
+        Route::resource('users', UserController::class);
+    });
+
+    //edit
+    Route::middleware(['role:1,2,3,4'])->group(function () {
+        Route::get('users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+        Route::put('users/{user}', [UserController::class, 'update'])->name('users.update');
+    });
+
+    //view
+    Route::middleware(['role:1,2'])->group(function () {
+        Route::get('users', [UserController::class, 'index'])->name('users.index');
+        Route::get('users/{user}', [UserController::class, 'show'])->name('users.show');
     });
 });
